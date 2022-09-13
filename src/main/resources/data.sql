795db870937ca4086fe69339f55113b0affc6f7e
Jonas Gredig <jongredig@gmail.com>
2022-09-13 19:00:47 +0200

    add adjustment to DB Script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,22 @@
-CREATE TABLE IF NOT EXISTS USERS
+DROP TABLE IF EXISTS DIALOG;
+DROP TABLE IF EXISTS CUSTOMER;
+CREATE TABLE IF NOT EXISTS CUSTOMER
 (
-    id      INT NOT NULL,
-    consent BIT NOT NULL,
+    id      INT         NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(30) NOT NULL,
+    surname VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
-CREATE TABLE IF NOT EXISTS DIALOGS
+
+CREATE TABLE IF NOT EXISTS DIALOG
 (
-    id                   INT,
-    customerId           INT,
-    conversationLanguage VARCHAR(2),
-    conversationText     TEXT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (customerId) REFERENCES users (id)
+    technical_id          INT        NOT NULL AUTO_INCREMENT,
+    id                   INT        NOT NULL,
+    customer_id           INT        NOT NULL,
+    conversation_language VARCHAR(2) NOT NULL,
+    conversation_text     TEXT       NOT NULL,
+    consent              BIT        NOT NULL,
+    created              TIMESTAMP  NOT NULL,
+    PRIMARY KEY (technical_id),
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMER (id)
 );
