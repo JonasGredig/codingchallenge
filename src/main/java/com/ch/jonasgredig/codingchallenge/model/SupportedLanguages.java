299ed1cb6fda431b37f4053e0fea6f573977138f
Jonas Gredig <jongredig@gmail.com>
2022-09-13 19:53:47 +0200

    add SupportedLanguages with Static variables with all suppored languages

---
+++ src/main/java/com/ch/jonasgredig/codingchallenge/model/SupportedLanguages.java
@@ -0,0 +1,11 @@
+package com.ch.jonasgredig.codingchallenge.model;
+
+public class SupportedLanguages {
+    public static final String DE = "DE";
+    public static final String EN = "EN";
+    public static final String IT = "IT";
+    public static final String FR = "FR";
+
+    //Schweizer Deutsch
+    public static final String CH = "CH";
+}
